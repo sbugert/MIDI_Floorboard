b212597009401642b6cf19ae1a3d6eb7edfbfe07
Simon Bugert <simon.bugert@googlemail.com>
2014-10-14 08:47:04 +0200

    Changed pinout

--- MIDI_FloorBoard.ino
+++ MIDI_FloorBoard.ino
@@ -1,32 +1,32 @@
 #define S0 11
 #define S1 10
 #define S2 9
-#define Z 12
+#define Z 8
 
 #define INPUTS 16
 
 const int enablePins[] = {
-  5, 6};
+  6, 7};
 
 // =============================== User Settings ========================================
 
-#define MIDICHANNEL 0    // MIDI-Kanal: 0-15 für Kanal 1-16
+#define MIDICHANNEL 0    // MIDI channel: 0-15 for channels 1-16
 #define VELOCITY 69
 const unsigned char noteValue[INPUTS] = {
   60,62,64,65,
   67,69,71,72,
   74,76,77,79,
-  81,83,84,86};  // Array mit Midi-Noten
+  81,83,84,86};  // array of midi note values
 
 // ======================================================================================
 
-int expr = 0;
+//int expr = 0;
 
 boolean activeSwitch[INPUTS] = {
   HIGH,HIGH,HIGH,HIGH,
   HIGH,HIGH,HIGH,HIGH,
   HIGH,HIGH,HIGH,HIGH,
-  HIGH,HIGH,HIGH,HIGH};     // gerade gedrückte Taster
+  HIGH,HIGH,HIGH,HIGH};     // currently pressed toggles
 
 void setup() {
   // Adress-Bits
@@ -41,23 +41,23 @@ void setup() {
   digitalWrite(enablePins[1], HIGH);
 
   pinMode(Z, INPUT);
-  digitalWrite(Z, HIGH); // internen Pull-Up aktivieren
+  digitalWrite(Z, HIGH); // activate internal pull-up
   
-//  analogReadResolution(8);    // ADC auf 8-Bit-Auflösung setzen
+//  analogReadResolution(8);
 
   Serial.begin(31250);
 }
 
 void loop() {
   for (int i = 0; i < INPUTS; i++) {
-    if(readSwitch(i) == LOW && activeSwitch[i] == HIGH) {             // Wenn Taster i gedrückt ist
+    if(readSwitch(i) == LOW && activeSwitch[i] == HIGH) {             // if toggle i pressed
       activeSwitch[i] = LOW;
-      sendMIDI(144, noteValue[i], VELOCITY);    // Note-On
+      sendMIDI(144, noteValue[i], VELOCITY);    // note-on
       continue;
     }
     if(readSwitch(i) == HIGH && activeSwitch[i] == LOW) {
         activeSwitch[i] = HIGH;
-        sendMIDI(128, noteValue[i], VELOCITY);    // Note-Off 128
+        sendMIDI(128, noteValue[i], VELOCITY);    // note-off 128
     }
   }
  //     if(expr != map(analogRead(0), 0, 1023, 0, 127)) {
